23667bfcc9876be63ad65a13d7ce9bc043aead3b
r0ger <rogeriomaiajr13@gmail.com>
2021-05-27 18:13:13 -0300

    Adiciona o index.html do exercicio

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ let whereAmI = document.querySelector('#elementoOndeVoceEsta').parentNode;
 console.log(whereAmI);
 
 whereAmI.style.color = 'red';
+
+let firtsSonofSon = whereAmI.firstChild;
+console.log(firtsSonofSon);
+
+firtsSonofSon.innerText = 'primeirofilho';
